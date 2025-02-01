f63501908da1efb45f2e856fee55a86c2a977dff
Paras Sharma <121804882+Parass09@users.noreply.github.com>
2025-02-01 14:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (28.64%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static void reverseString(char[] s) {
+   int left = 0, right = s.length - 1;
+        while (left < right) {
+            // Swap characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            // Move pointers
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        System.out.println(s); // Output: olleh
+    }
+}
